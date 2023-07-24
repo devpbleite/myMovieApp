3a100eb874f3fdb526962215be6924a648cb1c15
Pablo Leite <pabloleite03@gmail.com>
2023-07-24 17:28:00 -0300

    Update Constants.kt

--- app/src/main/java/app/kotlin/devpbleite/mymovieapp/utils/Constants.kt
+++ app/src/main/java/app/kotlin/devpbleite/mymovieapp/utils/Constants.kt
@@ -2,7 +2,7 @@ package app.kotlin.devpbleite.mymovieapp.utils
 
 object Constants {
 
-  const val API_KEY="7bbe6c33f7200695a00eeed84fcb5fa9"
+  const val API_KEY="put your api key"
   const val BASEURL="https://api.themoviedb.org/3/"
   const val POSTER_BASE_URL="https://image.tmdb.org/t/p/w500"
 
