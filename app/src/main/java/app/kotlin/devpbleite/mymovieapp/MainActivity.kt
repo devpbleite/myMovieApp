1b4d9273195de19ba454aefe621ce17582217ba7
Pablo Leite <pabloleite03@gmail.com>
2023-07-24 21:22:14 -0300

    Upgrade the project.

--- app/src/main/java/app/kotlin/devpbleite/mymovieapp/MainActivity.kt
+++ app/src/main/java/app/kotlin/devpbleite/mymovieapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package app.kotlin.devpbleite.mymovieapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.kotlin.devpbleite.mymovieapp.adapter.MoviesAdapter
 import app.kotlin.devpbleite.mymovieapp.api.ApiClient
 import app.kotlin.devpbleite.mymovieapp.api.ApiService
@@ -11,7 +13,6 @@ import app.kotlin.devpbleite.mymovieapp.response.MovieListResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
   
@@ -34,11 +35,41 @@ class MainActivity : AppCompatActivity() {
           call: Call<MovieListResponse>,
           response: Response<MovieListResponse>
         ) {
-          TODO("Not yet implemented")
+          progressBar.visibility = View.GONE
+          when(response.code()) {
+            // Suceessful response
+            in 200..299 -> {
+              response.body().let { itBody ->
+                itBody?.results.let { itData ->
+                  if(itData!!.isNotEmpty()) {
+                    movieAdapter.differ.submitList(itData)
+                    rvMovie.apply {
+                      layoutManager = LinearLayoutManager(this@MainActivity)
+                      adapter = movieAdapter
+                    }
+                  }
+                }
+
+              }
+            }
+            // Redirection response
+            in 300..399 -> {
+              Log.d("Response Code", " Redirection response : ${response.code()}")
+            }
+            // Client error response
+            in 400..499 -> {
+              Log.d("Response Code", " Client error response : ${response.code()}")
+            }
+            // Server error response
+            in 500..599 -> {
+              Log.d("Response Code", " Server error response : ${response.code()}")
+            }
+          }
         }
   
         override fun onFailure(call: Call<MovieListResponse>, t: Throwable) {
-          TODO("Not yet implemented")
+          binding.progressBar.visibility = View.GONE
+          Log.e("onFailure", "Error : ${t.message}")
         }
   
       })
